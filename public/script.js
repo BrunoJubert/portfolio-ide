7e53a752d8ef67c15223b0907666bd809f3db6be
BrunoJubert <jubert.bruno@oclock.school>
2025-02-13 12:27:21 +0100

    date générée dynamiquement dans le footer

---
+++ public/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+// DATE DYNAMICALLY GENERATED IN FOOTER // 
+document.addEventListener("DOMContentLoaded", function () {
+  const currentYearSpan = document.getElementById("current-year");
+  const currentYear = new Date().getFullYear();
+  currentYearSpan.textContent = currentYear;
+});
